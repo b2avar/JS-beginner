5bd6c90aa1d8dd144cefd889ced5391f84d2e04b
Burak Avar <burakavar@gmail.com>
2023-10-31 23:16:38 +0300

    foreach, map, filter, conditionals

--- main.js
+++ main.js
@@ -1,134 +1,160 @@
-//let, const
+// //let, const
+//
+// //let
+// let myAge = 30;
+// myAge = 31;
+// console.log(myAge);
+//
+// //const
+// const score = 20;
+// console.log(score);
+//
+// //String, Numbers, Boolean, null, undefined
+//
+// const name = 'Burak'; //string
+// const age = 31; //number
+// const rating = 4.5; //number
+// const isCool = true; //boolean
+// const x = null; //null - empty , object
+// const y = undefined; //undefined
+// let z; //undefined
+//
+// console.log(typeof z);
+//
+// //Concatenation
+// console.log('My name is ' + name + ' and I am ' + age); //old way
+//
+// //Template String
+// console.log(`My name is ${name} and I am ${age}`); //template string
+//
+// const hello = `My name is ${name} and I am ${age}`; //template string
+// //console.log(hello);
+//
+// //String Properties and Methods
+// const s = 'Hello World!';
+// console.log(s.length); //length of string
+// console.log(s.toUpperCase()); //uppercase
+// console.log(s.toLowerCase()); //lowercase
+// console.log(s.substring(0, 5)); //substring
+// console.log(s.substring(0, 5).toUpperCase()); //substring and uppercase
+// console.log(s.split('')); //split by character
+//
+// const t = 'technology, computers, it, code'; //split by comma
+// console.log(t.split(', ')); //split by character
+//
+//
+// //Arrays - variables that hold multiple values
+// const numbers = new Array(1,2,3,4,5); //constructor
+// console.log(numbers);
+//
+// //Fruits Array
+// const fruits = ['apples', 'oranges', 'pears', 10, true]; //can hold different types
+// console.log(fruits);
+// console.log(fruits[1]);
+//
+// fruits[3] = 'grapes'; //add to index
+// console.log(fruits);
+//
+// fruits.push('mangos'); //add to end
+// console.log(fruits);
+//
+// fruits.unshift('strawberries'); //add to front
+// console.log(fruits);
+//
+// fruits.pop(); //remove last
+// console.log(fruits);
+//
+// console.log(Array.isArray(fruits)); //check if array
+//
+// console.log(fruits.indexOf('oranges')); //check index
+//
+// //Object Literals
+// const person = {
+//     firstName: 'Burak',
+//     lastName: 'Avar',
+//     age: 31,
+//     hobbies: ['music', 'movies', 'sports'],
+//     address: {
+//         street: '50 main st',
+//         city: 'Boston',
+//         state: 'MA'
+//     }
+// }
+//
+// console.log(person);
+// //alert(person);
+//
+// console.log(person.firstName, person.lastName); //access properties
+// console.log(person.hobbies[1]); //access array
+// console.log(person.address.city); //access object
+//
+// const { firstName, lastName, address: { city } } = person; //destructuring
+// console.log(firstName);
+// console.log(city);
+//
+// person.email = 'burakavar@gmail.com'; //add property
+// console.log(person);
+// console.log(Object.entries(person)); //object to array
+//
+// const todos = [{
+//     id: 1,
+//     text: 'Take out trash',
+//     isCompleted: true
+// },
+// {
+//     id: 2,
+//     text: 'Meeting with boss',
+//     isCompleted: true
+// },
+// {
+//     id: 3,
+//     text: 'Dentist appointment',
+//     isCompleted: false
+// }];
+//
+// console.log(todos);
+// console.log(todos[1].text); //access array
+//
+// // const todoJSON = JSON.stringify(todos); //convert to JSON
+// // console.log(todoJSON);
+//
+// // For
+// for(let i = 0; i < todos.length; i++) {
+//     console.log(todos[i].id);
+// }
+//
+// // For Of
+// for(let todo of todos) {
+//     console.log(todo.text);
+// }
+//
+//
+// // While
+// let i = 0;
+// while (i < 10) {
+//     console.log(`While Loop Number: ${i}`);
+//     i++;
+// }
+//
+// // forEach, map, filter
+// const todoText = todos.map(function (todo) {
+//     return todo.text;
+// });
+// console.log(todoText);
+//
+// const todoCompleted = todos.filter(function (todo) {
+//     return todo.isCompleted === true;
+// }).map(function (todo) {
+//     return todo.text;
+// });
+// console.log(todoCompleted);
 
-//let
-let myAge = 30;
-myAge = 31;
-console.log(myAge);
-
-//const
-const score = 20;
-console.log(score);
-
-//String, Numbers, Boolean, null, undefined
-
-const name = 'Burak'; //string
-const age = 31; //number
-const rating = 4.5; //number
-const isCool = true; //boolean
-const x = null; //null - empty , object
-const y = undefined; //undefined
-let z; //undefined
-
-console.log(typeof z);
-
-//Concatenation
-console.log('My name is ' + name + ' and I am ' + age); //old way
-
-//Template String
-console.log(`My name is ${name} and I am ${age}`); //template string
-
-const hello = `My name is ${name} and I am ${age}`; //template string
-//console.log(hello);
-
-//String Properties and Methods
-const s = 'Hello World!';
-console.log(s.length); //length of string
-console.log(s.toUpperCase()); //uppercase
-console.log(s.toLowerCase()); //lowercase
-console.log(s.substring(0, 5)); //substring
-console.log(s.substring(0, 5).toUpperCase()); //substring and uppercase
-console.log(s.split('')); //split by character
-
-const t = 'technology, computers, it, code'; //split by comma
-console.log(t.split(', ')); //split by character
-
-
-//Arrays - variables that hold multiple values
-const numbers = new Array(1,2,3,4,5); //constructor
-console.log(numbers);
-
-//Fruits Array
-const fruits = ['apples', 'oranges', 'pears', 10, true]; //can hold different types
-console.log(fruits);
-console.log(fruits[1]);
-
-fruits[3] = 'grapes'; //add to index
-console.log(fruits);
-
-fruits.push('mangos'); //add to end
-console.log(fruits);
-
-fruits.unshift('strawberries'); //add to front
-console.log(fruits);
-
-fruits.pop(); //remove last
-console.log(fruits);
-
-console.log(Array.isArray(fruits)); //check if array
-
-console.log(fruits.indexOf('oranges')); //check index
-
-//Object Literals
-const person = {
-    firstName: 'Burak',
-    lastName: 'Avar',
-    age: 31,
-    hobbies: ['music', 'movies', 'sports'],
-    address: {
-        street: '50 main st',
-        city: 'Boston',
-        state: 'MA'
-    }
+// Conditionals
+const value = 10;
+if (value === 10) {
+    console.log('value is 10');
 }
 
-console.log(person);
-//alert(person);
-
-console.log(person.firstName, person.lastName); //access properties
-console.log(person.hobbies[1]); //access array
-console.log(person.address.city); //access object
-
-const { firstName, lastName, address: { city } } = person; //destructuring
-console.log(firstName);
-console.log(city);
-
-person.email = 'burakavar@gmail.com'; //add property
-console.log(person);
-console.log(Object.entries(person)); //object to array
-
-const todos = [{
-    id: 1,
-    text: 'Take out trash',
-    isCompleted: true
-},
-{
-    id: 2,
-    text: 'Meeting with boss',
-    isCompleted: true
-},
-{
-    id: 3,
-    text: 'Dentist appointment',
-    isCompleted: false
-}];
-
-console.log(todos);
-console.log(todos[1].text); //access array
-
-// const todoJSON = JSON.stringify(todos); //convert to JSON
-// console.log(todoJSON);
-
-// For
-for(let i = 0; i <=10; i++) {
-    console.log(`For Loop Number: ${i}`);
-}
-
-// While
-let i = 0;
-while (i < 10) {
-    console.log(`While Loop Number: ${i}`);
-    i++;
-}
 
 
 
