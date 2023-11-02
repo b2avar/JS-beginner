f6ff9b84771e4c0149d508f1a8b70539c254efe3
Burak Avar <burakavar@Buraks-iMac.local>
2023-11-02 15:02:56 +0300

    Listen for submit

--- main.js
+++ main.js
@@ -271,11 +271,12 @@
 // ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
 // ul.lastElementChild.remove();
 // ul.firstElementChild.style.background = 'black';
-//
+
+
 // const button = document.querySelector('.btn');
 // button.style.background = 'green';
-
-// Events
+//
+// // Events
 // button.addEventListener('mouseout', (e) => {
 //     e.preventDefault();
 //     console.log('click');
@@ -284,3 +285,30 @@
 //     document.querySelector('body').classList.add('bg-dark');
 //     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
 // });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for submit
+myForm.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+    e.preventDefault();
+
+    if (nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
